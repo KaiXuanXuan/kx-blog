373819fdf154c5d7a9fc5544a8b2700c66e9b94a
KaiXuanXuan <106977978+KaiXuanXuan@users.noreply.github.com>
2025-06-23 11:33:46 +0800

    还是蓝色好看

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -8,7 +8,7 @@
       <div class="max-w-7xl mx-auto px-4 py-8 bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 rounded-sm">
         <div class="mb-4 flex flex-col sm:flex-row items-center justify-between">
           <div class="text-xl font-medium mb-2 sm:mb-0 w-full sm:w-auto text-center sm:text-left flex flex-col md:flex-row items-center justify-center gap-8">
-            <p class="text-gray-200 bg-purple-600 rounded-2xl px-2 py-1">🚀点击"报告助手"，帮你自动总结报告</p>
+            <p class="text-gray-200 bg-blue-600 rounded-2xl px-2 py-1">🚀点击"报告助手"，帮你自动总结报告</p>
             <p>🔊:今天也要加油鸭💪</p>
           </div>
           <div class="flex gap-4 w-full sm:w-auto justify-center sm:justify-end">
